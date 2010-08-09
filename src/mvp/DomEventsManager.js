93a9530e2773f349176086879c3ba88dccb9070d
Andrea Giardina <agiardina@agiardina.local>
2010-08-09 08:23:54 +0100

    EventsManager added

---
+++ src/mvp/DomEventsManager.js
@@ -0,0 +1,30 @@
+jab.DomEventsManager = function() {
+    var manager = new jab.EventsManager();
+
+
+    /**
+     * @param {String} evName The event to handle
+     * @param {Function} handler The event handler
+     */
+    manager.addEventListener = function(evName,handler) {
+        jab.EventsManager.prototype.addEventListener.apply(this,arguments);
+        if (!this.hasEventListener(evName)) {
+            var self = this;
+            this.node().addEventListener(evName, function() {
+                self.fireEvent(evName);
+            });
+        }
+    }
+
+    /**
+     * The constructor
+     */
+    manager.constructor = function() {
+        jab.EventsManager.apply(this,arguments);
+    }
+
+    manager.constructor.prototype = manager;
+
+    return manager.constructor;
+}();
+
