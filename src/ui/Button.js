1f980e00caab2f3160eac3233ec8f4030f03dbd7
Andrea Giardina <agiardina@agiardina.local>
2010-09-12 15:28:58 +0100

    button notice and iconText added

--- src/ui/Button.js
+++ src/ui/Button.js
@@ -40,6 +40,43 @@ jab.ui.Button = function() {
 
         return this;
     };
+
+    button.notice = function(notice) {
+        if (!this._notice) {
+            if (!this._label) {
+                throw "Impossible set a notice without a label";
+            }
+            this._notice = new jab.html.Element('sup');
+            this._notice.addClass('notice');
+            this._label.appendChild(this._notice);
+        }
+
+        if (typeof notice != 'udefined') {
+            this._notice.content(notice);
+            return this;
+        } else {
+            return this._notice.content();
+        }
+    };
+
+    button.iconText = function(text) {
+        if (!this._icontext) {
+            if (!this._icon) {
+                throw "Impossible set icontext without an icon";
+            }
+            this._icontext = new jab.html.Element('span');
+            this._icontext.addClass('icontext');
+            this._icon.appendChild(this._icontext);
+        }
+
+        if (typeof text != 'undefined'){
+            this._icontext.content(text);
+            return this;
+        } else {
+            return this._icontext.content();
+        }
+
+    };
     
 
     button.constructor.prototype = button;
